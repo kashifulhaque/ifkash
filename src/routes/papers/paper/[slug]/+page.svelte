de7da65be6ce06fb95a0f2479389795458fba662
kashifulhaque <haque.kashiful7@gmail.com>
2024-10-17 16:26:02 +0530

    [WIP] Making a page for the paper

--- src/routes/papers/paper/[slug]/+page.svelte
+++ src/routes/papers/paper/[slug]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import { onMount } from "svelte";
 
   interface Author {
     name: string;
@@ -12,42 +13,43 @@
     dataset_url: string;
     arxiv_page_url: string;
     arxiv_pdf_url: string;
-    description: string
+    description: string;
   }
 
   let error = {
-    isError: false;
-    message: ""
-  }
+    isError: false,
+    message: "",
+  };
   let loading: boolean = true;
   let paper: Paper;
 
   $: slug = $page.params.slug;
-  if(slug.length == 0) {
-    error.isError = true,
-    error.message = "slug not found"
+  if (slug.length == 0) {
+    (error.isError = true), (error.message = "slug not found");
   }
 
-  const paperUrl = `https://paperswithcode.com/paper/${slug}`;
-  try {
-    const response = await fetch(`/api/pwc/paper?paper_url=${paperUrl}`);
-    if(!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
+  onMount(async () => {
+    const paperUrl = `https://paperswithcode.com/paper/${slug}`;
+    try {
+      const response = await fetch(`/api/pwc/paper?paper_url=${paperUrl}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
 
-    if (!data) {
-      throw new Error("Invalid data received from API");
-    }
+      if (!data) {
+        throw new Error("Invalid data received from API");
+      }
 
-    paper = data;
-  } catch(err) {
-    error.isError = true;
-    error.message = err as string;
-    console.error("Error fetching papers", error);
-  } finally {
-    loading = false;
-  }
+      paper = data;
+    } catch (err) {
+      error.isError = true;
+      error.message = err as string;
+      console.error("Error fetching papers", error);
+    } finally {
+      loading = false;
+    }
+  });
 </script>
 
 <h1>{paper}</h1>

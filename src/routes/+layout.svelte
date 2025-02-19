8cb5e7f50e725883c49d43034923604e86dc680a
kashifulhaque <haque.kashiful7@gmail.com>
2025-02-19 23:23:09 +0530

    Added vercel speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,9 @@
   import "../app.css";
   import { dev } from "$app/environment";
   import { injectAnalytics } from "@vercel/analytics/sveltekit";
+  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
 
+  injectSpeedInsights();
   injectAnalytics({
     mode: dev ? "development" : "production"
   });

f820f748b003b04cd7afcf3aeb8c9176c20cba27
kashifulhaque <haque.kashiful7@gmail.com>
2025-02-19 23:21:33 +0530

    Added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
   import "../app.css";
+  import { dev } from "$app/environment";
+  import { injectAnalytics } from "@vercel/analytics/sveltekit";
+
+  injectAnalytics({
+    mode: dev ? "development" : "production"
+  });
 </script>
 
 <slot />
